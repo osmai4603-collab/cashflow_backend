589e4b0e2c7e3be9cf61625d9d0117ac1b7c7fe5
osmai4603-collab <osmai4603@gmail.com>
2026-09-11 23:12:13 +0300

    7.80

---
+++ migrations/000069_quality_surveys_repair_planning_schema.down.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS planning_shifts;
+DROP TABLE IF EXISTS planning_roles;
+DROP TABLE IF EXISTS repair_order_lines;
+DROP TABLE IF EXISTS repair_orders;
+DROP TABLE IF EXISTS survey_questions;
+DROP TABLE IF EXISTS survey_surveys;
+DROP TABLE IF EXISTS quality_alerts;
+DROP TABLE IF EXISTS quality_control_points;
